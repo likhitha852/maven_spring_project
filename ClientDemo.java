d3fae98eb956ed5f15f17ba348a55bffc765f102
likhitha852 <likhithaaaareddy@gmail.com>
2024-12-07 12:15:00 +0530

    Add files via upload

---
+++ ClientDemo.java
@@ -0,0 +1,16 @@
+package com.klef.jfsd.exam;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ClientDemo {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Employee employee = (Employee) context.getBean("employee");
+        System.out.println(employee);
+
+        Course course = (Course) context.getBean("course");
+        System.out.println(course);
+    }
+}
